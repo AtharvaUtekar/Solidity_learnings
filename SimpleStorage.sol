84ca7498d35eb017d8931db9709c904218f84927
AtharvaUtekar <atharva.v.utekar@gmail.com>
2023-01-22 01:08:46 +0530

    First deployment to testnet

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,8 +3,10 @@
 pragma solidity ^0.8.7;
 
 contract SimpleStorage{
-    uint256  public favNumber;
+    // initializing a variable
+    uint256  favNumber;
 
+    // Function to store number
     function store (  uint _favNum ) public{
      favNumber=_favNum;
     }
@@ -12,19 +14,34 @@ contract SimpleStorage{
     // Created a struct
     People  public person  = People ({favNumber:12345, Name:"Atharva"});
 
+    // Defining the struct 
     struct  People {
         uint256  favNumber;
         string Name;
-
     }
 
     // Creating an array
-    People[] public 
+    // uint256[] public  favNumberList;
+    People[] public  people;
 
     // Getter function created by user
     function retrieve() public view returns  (uint){
         return favNumber;
     }
+
+    // Function to add to array
+    function addPerson (string memory _Name, uint256 _favNumber ) public {
+        // Another  method
+        // People memory newPerson = People({ favNumber: _favNumber, Name: _Name });
+        // people.push(newPerson);
+        people.push(People(_favNumber, _Name));
+        // Adding mapping to array
+        nameToFavNum[_Name]=_favNumber;
+    }
+
+    // Creating a map  String  name is mapped to fav number
+    mapping(string => uint256) public  nameToFavNum;
+
 }
 
 
