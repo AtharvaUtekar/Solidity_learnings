0858d88778514222eec1967ab2b76c1ec0395aed
AtharvaUtekar <atharva.v.utekar@gmail.com>
2023-01-22 11:44:36 +0530

    Updated code

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,50 +1,42 @@
 // You have to  always mention the version of solidity used because  sol is evloving language
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
 
 // First Contract address deployed on goerli test net network: 0x89801D2d74c1Bb46Bd3F1ce324b2B0688D54Bf2e
 
-contract SimpleStorage{
+contract SimpleStorage {
     // initializing a variable
-    uint256  favNumber;
+    uint256 favoriteNumber;
+    // Defining the struct 
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // uint256[] public anArray;
+    People[] public people;
+
+    // Creating a map  String  name is mapped to fav number
+    mapping(string => uint256) public nameToFavoriteNumber;
 
     // Function to store number
-    function store (  uint _favNum ) public{
-     favNumber=_favNum;
+    function store(uint256 _favoriteNumber) public virtual {
+        favoriteNumber = _favoriteNumber;
     }
-
-    // Created a struct
-    People  public person  = People ({favNumber:12345, Name:"Atharva"});
-
-    // Defining the struct 
-    struct  People {
-        uint256  favNumber;
-        string Name;
-    }
-
-    // Creating an array
-    // uint256[] public  favNumberList;
-    People[] public  people;
-
-    // Getter function created by user
-    function retrieve() public view returns  (uint){
-        return favNumber;
+    
+    // Getter function created by me
+    function retrieve() public view returns (uint256){
+        return favoriteNumber;
     }
 
     // Function to add to array
-    function addPerson (string memory _Name, uint256 _favNumber ) public {
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
         // Another  method
         // People memory newPerson = People({ favNumber: _favNumber, Name: _Name });
         // people.push(newPerson);
-        people.push(People(_favNumber, _Name));
+        people.push(People(_favoriteNumber, _name));
         // Adding mapping to array
-        nameToFavNum[_Name]=_favNumber;
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
-
-    // Creating a map  String  name is mapped to fav number
-    mapping(string => uint256) public  nameToFavNum;
-
 }
-
-
-// 0xd9145CCE52D386f254917e481eB44e9943F39138
--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,19 +1,27 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT 
+
 pragma solidity ^0.8.7;
 
-import "./SimpleStorage.sol";
+import "./SimpleStorage.sol"; 
 
 contract StorageFactory {
-    // Create a copy of SimpleStorage
-    SimpleStorage[] public  simpleStorageArray;
-
-    // Function to create simple storage
-    function createSimpleStorageContract () public {
+    
+    SimpleStorage[] public simpleStorageArray;
+    
+    function createSimpleStorageContract() public {
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
     }
-
-    function sfStore() public{
-        
+    
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        // Address 
+        // ABI 
+        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+    
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
