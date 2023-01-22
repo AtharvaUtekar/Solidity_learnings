75c3e8f260b744ea0b658710a7b496e69a2b2966
AtharvaUtekar <atharva.v.utekar@gmail.com>
2023-01-22 11:26:22 +0530

    Making SimpleStorage array

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,15 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "./SimpleStorage.sol"
+import "./SimpleStorage.sol";
 
 contract StorageFactory {
     // Create a copy of SimpleStorage
-    SimpleStorage public  simpleStorage;
+    SimpleStorage[] public  simpleStorageArray;
 
     // Function to create simple storage
     function createSimpleStorageContract () public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
 
+    function sfStore() public{
+        
     }
 }
